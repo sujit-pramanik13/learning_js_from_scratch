6e273d8bb9b0a9d8e82ff31c785927199ccf7b5b
sujit-pramanik13 <117461428+sujit-pramanik13@users.noreply.github.com>
2023-09-30 05:47:23 +0000

    basics of variables

---
+++ jsTutorials/test.js
@@ -0,0 +1,15 @@
+console.log("sujit kumar pramanik ");
+const aadharNumber = 45244;
+let emailId = "javascript@gmail.com";
+var currentCity = " dhanbad " ;
+var accountPassword = "asd12";
+accountPassword = "254s"
+
+let isUserLoggedIn = true ;
+
+
+
+
+console.table([aadharNumber,emailId,currentCity,accountPassword,isUserLoggedIn]);
+console.log(typeof undefined);
+console.log(typeof null);
